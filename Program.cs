16e0cd4108b4d04bc6787f054c5fc25105ff8658
Konstantin <kostyahlamenkov@gmail.com>
2024-04-28 21:33:07 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Процесс запущен");
+        Thread.Sleep(2000);
+        Console.WriteLine("3");
+        Thread.Sleep(1000);
+        Console.WriteLine("2");
+        Thread.Sleep(1000);
+        Console.WriteLine("1");
+        Thread.Sleep(1000);
+        Console.WriteLine("Ракеты были запущены");
+    }
+}
